0b4f6c5deb3f808541a38318c224a58149bb5f0b
Pranjal Gajaya <gajayapranjal@gmail.com>
2025-11-14 19:35:49 +0530

    Configure for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Import the router
+import { HashRouter } from 'react-router-dom'; // Import the router
 import { AuthProvider } from './contexts/AuthContext'; // Import our new provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Makes routing work everywhere */}
+    <HashRouter> {/* Makes routing work everywhere */}
       <AuthProvider> {/* Provides user auth state to the whole app */}
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
